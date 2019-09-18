fb358d7443356fa30ca0bfa175f08c027ac6df4f
jamessouth <jamesdanielsouth@gmail.com>
2019-09-18 18:45:46 -0500

    cont demos

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import '../css/demo.scss';
 
+window.lastDiv = document.querySelector('div:last-of-type');
+
 if (CSS.paintWorklet) {
   CSS.paintWorklet.addModule('./demo1.min.js');
   CSS.paintWorklet.addModule('./demo2.min.js');
